b819b96e7ba1e20e67c24c6f21cf8f8a3196875d
harshalpatel <harshal.patel@raweng.com>
2018-07-03 19:13:33 +0530

    cors issue

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/ooyala', function(req, res) {
 
 	res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', '*');
 
 
 	api.get(req.url)
