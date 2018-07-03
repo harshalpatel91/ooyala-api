70a71f76c9850cbe9144c80169dce28c8bd26b51
harshalpatel <harshal.patel@raweng.com>
2018-07-03 18:04:05 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var os = require('os');
+var fs = require('fs');
+var app = express();
+var request = require('request');
+var path = require('path');
+var ooyalaApi = require('ooyala-api');
+
+var port = process.env.PORT || 5000;
+
+
+app.use('/ooyala', function(req, res) {
+	var ooyalaApiKey = req.headers.api_key;
+	var ooyalaSecretKey = req.headers.secret;
+	var api = new ooyalaApi(ooyalaApiKey, ooyalaSecretKey, {concurrency: 6});
+
+	res.setHeader('Content-Type', 'application/json');
+
+	api.get(req.url)
+		.then((body) => {
+			res.send(body);
+		})
+		.catch((err) => {
+		  	res.send(err);
+		});
+
+	
+
+});
+
+
+
+
+app.listen(port);
+
+
