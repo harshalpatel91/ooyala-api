43b09fe28ac4e789b0e31bbb4847238d1ea1fa47
harshalpatel <harshal.patel@raweng.com>
2018-07-16 15:40:23 +0530

    search api in ooyala

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 var request = require('request');
 var path = require('path');
 var ooyalaApi = require('ooyala-api');
+var Ooyala = require('ooyala');
 
 var port = process.env.PORT || 5000;
 
@@ -18,7 +19,6 @@ app.use('/ooyala', function(req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', '*');
 
-
 	api.get(req.url)
 		.then((body) => {
 			res.send(body);
@@ -26,9 +26,27 @@ app.use('/ooyala', function(req, res) {
 		.catch((err) => {
 		  	res.send(err);
 		});
+});
 
+app.use('/ooyala-search', function(req, res) {
+	var ooyalaApiKey = req.headers.api_key;
+	var ooyalaSecretKey = req.headers.secret;
+	var api = new ooyalaApi(ooyalaApiKey, ooyalaSecretKey, {concurrency: 6});
+	var search = "name='"+req.query.search+"' OR description='"+req.query.search+"'";
 	
+	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
 
+	api.get('/v2/assets', {where: search}, {recursive: true})
+		.then((items) => {
+		  	// All items in an array (recursive calls will be done internally)
+			res.send(items);
+		})
+		.catch((err) => {
+		  	// Error response
+			res.send(err);
+		});	
 });
 
 
